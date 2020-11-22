48adc8a003a4fff7fd3cf0db7065c7cd27443c5e
Bibi <bibi_agent000@msn.com>
2020-11-22 04:40:43 +0100

    log as MEDIA_USER to perform docker operations if current user is different. waiting for enter key to edit .env to let user see previous logs/errors.

--- init.sh
+++ init.sh
@@ -4,19 +4,14 @@ echo "[$0] Initializing..."
 
 if [[ ! -f .env ]]; then
   cp .env.sample .env
-  echo "[$0] Please edit .env file"
 fi
 
 # editing .env file
+read -s -p "Press {Enter} to edit the .env file"
 nano .env
 source .env
 
 # add new user
 ./init-add-user.sh
-
-echo "login as user $MEDIA_USER to perform docker operations"
-su $MEDIA_USER
-
-docker network create traefik-network 2>&1 || true
 
 echo "[$0] Done."
--- setup-fresh-install.sh
+++ setup-fresh-install.sh
@@ -8,8 +8,20 @@ chmod +x *.sh
 # init .env file and treafik
 ./init.sh
 
-# run our docker images
-./update-all.sh
+echo "login as user '$MEDIA_USER' to perform docker operations"
+CUR_USER=$(whoami)
 
+if [ $CUR_USER != $MEDIA_USER ]; then
+    # log as MEDIA_USER, add the traefik network and run our docker images
+exec sudo -i -u $MEDIA_USER /bin/bash - << eof
+    docker network create traefik-network 2>&1 || true
+    ./update-all.sh
+    exit 0
+eof
+else
+    docker network create traefik-network 2>&1 || true
+    ./update-all.sh
+fi
+
+# back as previous user
 echo "[$0] Done."
-exit 0
