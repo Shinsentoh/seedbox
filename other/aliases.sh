4643d28e70b9c97b8b68b3ec375dc228d64d9f3c
Bibi <bibi_agent000@msn.com>
2020-11-28 01:46:44 +0100

    removing command on nextcloud-db

--- other/aliases.sh
+++ other/aliases.sh
@@ -5,3 +5,5 @@
 alias all-logs="docker ps -q | xargs -P 13 -L 1 docker logs --follow"
 alias dockerStopAll="docker stop $(docker ps -a -q)"
 alias dockerStartAll="docker start $(docker ps -a -q)"
+alias dcUp="docker-compose up -d --remove-orphans"
+alias dcDown="docker-compose down --remove-orphans"
