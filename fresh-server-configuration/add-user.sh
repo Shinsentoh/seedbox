af6ce0a0e7c25665f961328b4b87211425ab3e59
Shinsentoh <bibi_agent000@msn.com>
2023-03-31 23:34:18 +0200

    if doesn't work

--- fresh-server-configuration/add-user.sh
+++ fresh-server-configuration/add-user.sh
@@ -7,17 +7,17 @@ fi
 source ./fresh-server-configuration/.serverEnv
 
 if [ "$ADD_USER" = true ] ; then
-    if [ ! id "$SYSTEM_USER" &>/dev/null ] ; then
+    # if id -u "$SYSTEM_USER" >/dev/null 2>&1; then
         # creates user and his home directory
         /usr/sbin/adduser $SYSTEM_USER
         # add user to group sudo to be able to run root commands
         usermod -aG sudo $SYSTEM_USER
-    fi
+    # fi
 fi
 
 if [ "$ADD_ALIASES_TO_USER_SHELL" = true ] ; then
-    if [ id "$SYSTEM_USER" &>/dev/null ] ; then
+    # if id -u "$SYSTEM_USER" >/dev/null 2>&1; then
         # copy aliases into the user bashrc to have them for later
         cat ./fresh-server-configuration/aliases.sh >> /home/$SYSTEM_USER/.bashrc
-    fi
+    # fi
 fi
